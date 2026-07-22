1b198c72ed2a2796bbbc316b3e9725743c426c76
rpnny <159451060+rpnny@users.noreply.github.com>
2026-07-22 15:13:35 +0200

    Pin Foundry dependencies by exact revision

--- contracts/scripts/install-deps.sh
+++ contracts/scripts/install-deps.sh
@@ -5,8 +5,8 @@ CONTRACTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$CONTRACTS_DIR"
 
 forge install --no-git --shallow \
-  v4-core=Uniswap/v4-core@v1.0.2 \
-  v4-periphery=Uniswap/v4-periphery@v1.0.4 \
-  forge-std=foundry-rs/forge-std@v1.16.1 \
-  eas-contracts=ethereum-attestation-service/eas-contracts@v1.9.0 \
-  openzeppelin-contracts=OpenZeppelin/openzeppelin-contracts@v5.6.1
+  v4-core=Uniswap/v4-core@rev=59d3ecf53afa9264a16bba0e38f4c5d2231f80bc \
+  v4-periphery=Uniswap/v4-periphery@rev=3231810e39b8c4d569b9d66907fa4ef8cd2cec22 \
+  forge-std=foundry-rs/forge-std@rev=620536fa5277db4e3fd46772d5cbc1ea0696fb43 \
+  eas-contracts=ethereum-attestation-service/eas-contracts@rev=3683c3ec9383091eebd6f183e67b485e09a53dd7 \
+  openzeppelin-contracts=OpenZeppelin/openzeppelin-contracts@rev=5fd1781b1454fd1ef8e722282f86f9293cacf256
